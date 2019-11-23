4c6d7ffbf477b944542bc8eba8a778727e1dca14
Jenni Hay <finalfantasyguru8705@gmail.com>
2019-11-22 21:47:11 -0600

    Update GoogleCivicAPI.java

--- app/src/main/java/com/csc472/hw4knowyourgov/model/GoogleCivicAPI.java
+++ app/src/main/java/com/csc472/hw4knowyourgov/model/GoogleCivicAPI.java
@@ -14,18 +14,21 @@ import org.springframework.web.client.RestTemplate;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import static java.lang.String.format;
+
 public class GoogleCivicAPI  extends AsyncTask<String, Void, String> {
 
     private static final String TAG = "GoogleCivicAPI";
+    private final String noDataFound = "There is no available Data";
 
     private static final String API_KEY = "AIzaSyBSwoiBNOd6mOPi8ThzbFEUd6B5ZVsduyw";
 
     private static final String GOOGLE_CIVIC = "https://www.googleapis.com/civicinfo/v2/representatives?key="+API_KEY+"&address=";
 
-    private final String noData = "There is no available Data";
+
 
     private ArrayList<Official> officialResults = new ArrayList<Official>();
-    private Object[] results = new Object[2];
+    private Object[] civicResults = new Object[2];
     private String location;
 
     private MainActivity mainActivity;
@@ -36,32 +39,27 @@ public class GoogleCivicAPI  extends AsyncTask<String, Void, String> {
 
 
     @Override
-    protected void onPostExecute(String s) {
+    protected void onPostExecute(String dataToSend) {
         Log.d(TAG, "onPostExecute: ");
 
-        if (s == null) {
+        if (dataToSend == null) {
             Toast.makeText(mainActivity, "Civic Info service is unavailable", Toast.LENGTH_SHORT).show();
             mainActivity.updateOfficalList(null);
-            return;
 
-        } else if (s.isEmpty()) {
+        } else if (dataToSend.isEmpty()) {
             Toast.makeText(mainActivity, "No data is available for the specified location", Toast.LENGTH_SHORT).show();
             mainActivity.updateOfficalList(null);
-            return;
 
         } else {
-            parseJSON(s);
-            results[0] = location;
-            results[1] = officialResults;
-            mainActivity.updateOfficalList(results);
-            return;
+            parseJSON(dataToSend);
+            civicResults[0] = location;
+            civicResults[1] = officialResults;
+            mainActivity.updateOfficalList(civicResults);
         }
     }
     @Override
     protected String doInBackground(String... strings) {
         String civicsURL = GOOGLE_CIVIC + strings[0];
-
-
         RestTemplate restTemplate = new RestTemplate();
         return restTemplate.getForObject(civicsURL, String.class);
     }
@@ -70,104 +68,114 @@ public class GoogleCivicAPI  extends AsyncTask<String, Void, String> {
     private void parseJSON(String s) {
         try {
             JSONObject jsonObject = new JSONObject(s);
-
-            JSONObject normalizedInputObject = jsonObject.getJSONObject("normalizedInput");
-            String city = normalizedInputObject.getString("city");
-            String state = normalizedInputObject.getString("state");
-            String zip = normalizedInputObject.getString("zip");
+            JSONObject normalizedInput = jsonObject.getJSONObject("normalizedInput");
+            String city = normalizedInput.getString("city");
+            String state = normalizedInput.getString("state");
+            String zip = normalizedInput.getString("zip");
             location = city + ", " + state + " " + zip;
-
-
             JSONArray officesArray = jsonObject.getJSONArray("offices");
-            HashMap<String, String> officeIndexHashMap = new HashMap<>();
-            for (int i = 0; i < officesArray.length(); i++) {
-                JSONObject officeObject = (JSONObject) officesArray.get(i);
-                String officialTitle = officeObject.getString("name");
-                JSONArray officialIndices = officeObject.getJSONArray("officialIndices");
+            HashMap<String, String> officialHashMap = new HashMap<>();
+            {
+                int i = 0;
+                while (i < officesArray.length()) {
+                    JSONObject officeObject = (JSONObject) officesArray.get(i);
+                    String officialTitle = officeObject.getString("name");
+                    JSONArray officialIndices = officeObject.getJSONArray("officialIndices");
 
-                for (int j = 0; j < officialIndices.length(); j++)
-                    officeIndexHashMap.put(officialIndices.getString(j), officialTitle);
-            }
-
-
-            JSONArray officialsArray= jsonObject.getJSONArray("officials");
-            for (int i = 0; i < officialsArray.length(); i++) {
-                JSONObject officialObject = (JSONObject) officialsArray.get(i);
-
-                String office = officeIndexHashMap.get(String.format("%d", i));
-                String name = officialObject.getString("name");
-
-                String address = "";
-                if (officialObject.has("address")) {
-                    JSONObject addressObject = (JSONObject) officialObject.getJSONArray("address").get(0);
-
-                    if (addressObject.has("line1"))
-                        address += addressObject.getString("line1") + "\n";
-                    else if (addressObject.has("line2"))
-                        address += ", " + addressObject.getString("line2") + "\n";
-                    address += addressObject.get("city") + ", " + addressObject.get("state") + " " + addressObject.get("zip");
-
-                } else {
-                    address = noData;
-                }
-
-                String party;
-                if (officialObject.has("party"))
-                    party = officialObject.getString("party");
-                else
-                    party = "Unkown";
-
-                String phone;
-                if (officialObject.has("phones"))
-                    phone = officialObject.getJSONArray("phones").get(0).toString();
-                else
-                    phone = noData;
-
-                String url;
-                if (officialObject.has("urls"))
-                    url = officialObject.getJSONArray("urls").get(0).toString();
-                else
-                    url = noData;
-
-                String email;
-                if (officialObject.has("emails"))
-                    email = officialObject.getJSONArray("emails").get(0).toString();
-                else
-                    email = noData;
-
-                String photoUrl;
-                if (officialObject.has("photoUrl"))
-                    photoUrl = officialObject.getString("photoUrl");
-                else
-                    photoUrl = noData;
-
-                String googlePlus = noData;
-                String facebook = noData;
-                String twitter = noData;
-                String youTube = noData;
-                if (officialObject.has("channels")) {
-                    JSONArray channelsArray = officialObject.getJSONArray("channels");
-                    for (int j = 0; j < channelsArray.length(); j++) {
-                        JSONObject channelObject = (JSONObject) channelsArray.get(j);
-                        if (channelObject.getString("type").equals("GooglePlus"))
-                            googlePlus = channelObject.getString("id");
-
-                        else if (channelObject.getString("type").equals("Facebook"))
-                            facebook = channelObject.getString("id");
-
-                        else if (channelObject.getString("type").equals("Twitter"))
-                            twitter = channelObject.getString("id");
-
-                        else if (channelObject.getString("type").equals("YouTube"))
-                            youTube = channelObject.getString("id");
+                    int j = 0;
+                    while (j < officialIndices.length()) {
+                        officialHashMap.put(officialIndices.getString(j), officialTitle);
+                        j++;
                     }
+                    i++;
                 }
-
-                officialResults.add(new Official(office, name, party, address, phone, email, url, photoUrl, googlePlus, facebook, twitter, youTube));
             }
+            mapOfficialData(jsonObject, officialHashMap);
         } catch (JSONException e) {
             Log.d(TAG, "parseJSON: " + e.getMessage());
             e.printStackTrace();
+        }
+    }
+
+    private void mapOfficialData(JSONObject jsonObject, HashMap<String, String> officialHashMap) throws JSONException {
+        JSONArray officialsArray= jsonObject.getJSONArray("officials");
+        int i = 0;
+        while (i < officialsArray.length()) {
+            JSONObject official = (JSONObject) officialsArray.get(i);
+
+            String office = officialHashMap.get(format("%d", i));
+            String name = official.getString("name");
+
+            String officialAddress = "";
+            if (official.has("officialAddress")) {
+                JSONObject addressObject = (JSONObject) official.getJSONArray("officialAddress").get(0);
+
+                if (addressObject.has("line1")) {
+                    officialAddress.concat(addressObject.getString("line1") + "\n");
+                } else if (addressObject.has("line2"))
+                    officialAddress.concat(", " + addressObject.getString("line2") + "\n");
+                officialAddress.concat( addressObject.get("city") + ", " + addressObject.get("state") + " " + addressObject.get("zip"));
+
+            } else {
+                officialAddress = noDataFound;
+            }
+
+            String party;
+            if  (official.has("party"))
+                party = official.getString("party");
+            else
+                party = noDataFound;
+
+            String phone;
+            if (official.has("phones"))
+                phone = official.getJSONArray("phones").get(0).toString();
+            else
+                phone = noDataFound;
+
+            String url;
+            if (official.has("urls"))
+                url = official.getJSONArray("urls").get(0).toString();
+            else
+                url = noDataFound;
+
+            String email;
+            if (official.has("emails"))
+                email = official.getJSONArray("emails").get(0).toString();
+            else
+                email = noDataFound;
+
+            String photoUrl;
+            if (official.has("photoUrl"))
+                photoUrl = official.getString("photoUrl");
+            else
+                photoUrl = noDataFound;
+
+            String googlePlus = noDataFound;
+            String facebook = noDataFound;
+            String twitter = noDataFound;
+            String youTube = noDataFound;
+            if (official.has("channels")) {
+                JSONArray channelsArray = official.getJSONArray("channels");
+                int j = 0;
+                while (j < channelsArray.length()) {
+                    JSONObject channelObject = (JSONObject) channelsArray.get(j);
+                    if (channelObject.getString("type").equals("GooglePlus"))
+                        googlePlus = channelObject.getString("id");
+
+                    else if (channelObject.getString("type").equals("Facebook"))
+                        facebook = channelObject.getString("id");
+
+                    else if (channelObject.getString("type").equals("Twitter"))
+                        twitter = channelObject.getString("id");
+
+                    else if (channelObject.getString("type").equals("YouTube"))
+                        youTube = channelObject.getString("id");
+                    j++;
+                }
+            }
+
+            officialResults.add(new Official(office, name, party, officialAddress, phone, email, url, photoUrl, googlePlus, facebook, twitter, youTube));
+            i++;
         }
     }
 
